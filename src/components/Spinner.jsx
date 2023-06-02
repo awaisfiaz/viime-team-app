f4ebe25d3f904750c3fd9a9051b5fe871647df48
awaisfiaz <awaisfiaz360@gmail.com>
2023-06-02 21:06:06 +0500

    increased spinner size

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,7 +1,7 @@
 export default function Spinner() {
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-blue-500"></div>
+      <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-sidebar-bg"></div>
     </div>
   );
 }
